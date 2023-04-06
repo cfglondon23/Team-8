658824e8c1891cbe5aa179b15216126bd2bbf6c8
mrwilliambarenga <williambarenga@gmail.com>
2023-04-06 13:17:44 +0100

    Filter the dictionary from get_teleport_data

--- src/stats.py
+++ src/stats.py
@@ -1,5 +1,21 @@
 import requests
- 
+
+
+def get_useful_scores(city_name):
+
+    # Housing
+    # Travel connectivity
+    # Healthcare
+    # Environmental quality
+    # Cost of living
+    # Safety
+    # Education
+    # Economy
+    # Internet Access
+
+    return dict(filter(filter_dict, get_teleport_data(city_name)['scores'].items()))
+
+
 def get_teleport_data(city_name):
     ua_endpoint = get_urban_area_endpoint(city_name)
     results = {}
@@ -11,32 +27,33 @@ def get_teleport_data(city_name):
         scores = get_area_scores(ua_endpoint)
         results['scores'] = scores
     return results
- 
- 
+
+
 def get_urban_area_endpoint(city_name):
     try:
         cities_endpoint = 'https://api.teleport.org/api/cities/'
         url = f'{cities_endpoint}?search={city_name}'
-         
+
         r = requests.get(url)
         r = r.json()
         r['_embedded']
         geoname_url = r['_embedded']['city:search-results'][0]['_links']['city:item']['href']
-         
+
         r2 = requests.get(geoname_url)
         r2 = r2.json()
         ua_endpoint = r2['_links']['city:urban_area']['href']
         return ua_endpoint
     except:
         return None
- 
+
+
 def get_area_details(ua_endpoint):
     try:
         r = requests.get(ua_endpoint + 'details/')
         r = r.json()
- 
+
         result = {}
- 
+
         for cat in r['categories']:
             result[cat['label']] = {}
             for i in cat['data']:
@@ -48,8 +65,8 @@ def get_area_details(ua_endpoint):
         return result
     except:
         return 'not_found'
- 
- 
+
+
 def get_area_images(ua_endpoint):
     try:
         r = requests.get(ua_endpoint + 'images/')
@@ -57,8 +74,8 @@ def get_area_images(ua_endpoint):
         return r['photos']
     except:
         return 'not_found'
- 
- 
+
+
 def get_area_scores(ua_endpoint):
     try:
         r = requests.get(ua_endpoint + 'scores/')
@@ -71,3 +88,14 @@ def get_area_scores(ua_endpoint):
         return result
     except:
         return 'not_found'
+
+
+def filter_dict(pair):
+    key, value = pair
+    if key == 'Startups' or key == 'Venture Capital' or key == 'Commute' or key == 'Business Freedom' or key == 'Taxation' or key == 'Leisure & Culture' or key == 'Tolerance' or key == 'Outdoors':
+        return False
+    else:
+        return True
+
+
+print(get_useful_scores('London'))
