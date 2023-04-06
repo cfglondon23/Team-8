aee61f49c69e62e245bc6413930af6c95ec8465d
mrwilliambarenga <williambarenga@gmail.com>
2023-04-06 14:43:00 +0100

    Initialise the track page

--- src/main.py
+++ src/main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/track.html')
+def track():
+    return render_template('track.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
