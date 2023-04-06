68212403fe942a1bf8c9413702ae2e7547a7f321
mrwilliambarenga <williambarenga@gmail.com>
2023-04-06 14:48:40 +0100

    Fix navigation bar

--- src/main.py
+++ src/main.py
@@ -8,9 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/base.html')
+def base():
+    return render_template('base.html')
+
 @app.route('/track.html')
 def track():
     return render_template('track.html')
+
+@app.route('/ratings.html')
+def ratings():
+    return render_template('ratings.html')
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
